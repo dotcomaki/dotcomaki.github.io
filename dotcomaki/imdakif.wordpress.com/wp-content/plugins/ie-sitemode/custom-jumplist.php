bee6f9e4649fe391dabacfb3dccae5701b53d900
dotcomaki <114000157+dotcomaki@users.noreply.github.com>
2023-08-14 00:44:15 +0530

    Delete dotcomaki directory

--- dotcomaki/imdakif.wordpress.com/wp-content/plugins/ie-sitemode/custom-jumplist.php
+++
@@ -1,2 +0,0 @@
-function wpcom_custom_jumplist_reader(){window.external.msSiteModeCreateJumplist("Recent posts");window.external.msSiteModeAddJumpListItem("Hello World!","https://imdakif.wordpress.com/2023/08/13/hello-world-2/","https://s1.wp.com/i/favicon.ico");}
-try{if(window.external && 'msIsSiteMode' in window.external){if (window.external.msIsSiteMode()){wpcom_custom_jumplist_reader();wpcom_sitemode_comment_notify();}}}catch(e){}
